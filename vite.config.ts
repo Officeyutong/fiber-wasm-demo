00a42c483add151afd77b49fa313ae0c1220bc96
officeyutong <yt.xyxx@gmail.com>
2025-08-14 16:06:25 +0800

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,6 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
     }
-  }
+  },
+  base: "/fiber-wasm-demo/"
 })
